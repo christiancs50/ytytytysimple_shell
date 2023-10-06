2bec3ed3c34c4577025acd92988f0fa5ab3cd254
christiancs50 <christian.gaoso@gmail.com>
2023-10-06 08:31:56 -0700

    updated

--- shell.h
+++ shell.h
@@ -1,12 +1,18 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* heasderfiles */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
+/* prototypes */
 
-int str_len(char *s)
+int str_len(char *s);
+void shell_prompt(void);
+void run_command(const char *command)
 
 #endif /* SHELL_H */
