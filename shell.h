9f941f6b4e032065c1cfc992f8d38d6a7f66a3b2
christiancs50 <christian.gaoso@gmail.com>
2023-10-09 09:44:47 -0700

    headerfiles

--- shell.h
+++ shell.h
@@ -8,14 +8,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #define MAX_INPUT_SIZE 1024
+#define MAX_ARGUMENTS 50
 /* prototypes */
 
+int tokenize_command(char *command, char *args[]);
 int str_len(const char *s);
 void shell_prompt(void);
-void run_command(const char *command);
+void run_command(char *args[]);
 char *read_user_command(void);
 void _print(const char *message);
+void sig_handler(int signum);
+void execute_command_from_path(char *args[]);
 
 #endif /* SHELL_H */
