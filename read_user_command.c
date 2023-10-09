1172ea58e8b901b646ff66847fa35c1aa086a296
christiancs50 <christian.gaoso@gmail.com>
2023-10-09 13:48:12 -0700

    read_user_cmd

--- read_user_command.c
+++ read_user_command.c
@@ -1,16 +1,21 @@
 #include "shell.h"
 
 /**
- * read_user_command: Function that read user input
+ * read_user_command - Function that read user input
+ *
+ * this function reads a line from the stdio and returns as a
+ * dunamic allocated string.
+ *
+ * Return: Apointer to the user's input string, or Null on fail
  */
- 
+
 char *read_user_command(void)
 {
 	char *input;
 	size_t readbyte;
 	size_t input_Length;
 
-	input = (char*) malloc( MAX_INPUT_SIZE);
+	input = (char *) malloc(MAX_INPUT_SIZE);
 
 	if (input == NULL)
 	{
@@ -21,21 +26,21 @@ char *read_user_command(void)
 	shell_prompt();
 	readbyte = read(STDIN_FILENO, input, MAX_INPUT_SIZE);
 
-	if(readbyte == (size_t) -1)
+	if (readbyte == (size_t) -1)
 	{
 		perror("read");
 		free(input);
 		exit(EXIT_FAILURE);
 	}
 
-	if(readbyte == 0)
+	if (readbyte == 0)
 	{
 		free(input);
 		return (NULL);
 	}
 
 	input_Length = str_len(input);
-	if(input_Length > 0 && input[input_Length - 1] == '\n')
+	if (input_Length > 0 && input[input_Length - 1] == '\n')
 	{
 		input[input_Length - 1] = '\0';
 	}
