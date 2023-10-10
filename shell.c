b62a5076930aaacc08842aa74629458b92b4e67a
christiancs50 <christian.gaoso@gmail.com>
2023-10-10 09:13:29 -0700

    main_entry

--- shell.c
+++ shell.c
@@ -1,12 +1,17 @@
 #include "shell.h"
 
+/**
+ * main - Function is the entry point for my simple shell program
+ * Return: 0
+ */
+
 int main(void)
 {
 	int num_args;
 	char *args[MAX_ARGUMENTS];
 
 	signal(SIGINT, sig_handler);
-        signal(SIGSTOP, sig_handler);
+	signal(SIGSTOP, sig_handler);
 
 	while (1)
 	{
@@ -23,23 +28,20 @@ int main(void)
 			break;
 		}
 
+		if (strcmp(command, "env") == 0)
+		{
+			env_builtin();
+		}
+
 		num_args = tokenize_command(command, args);
 
 		if (num_args > 0)
 		{
-			if (command[0] == '/')
-			{
-				run_command(args);
-			}
-			else
-			{
-				execute_command_from_path(args);
-			}
+			run_command(args);
 		}
 
-	free(command);
+		free(command);
 	}
 
-    return 0;
+	return (0);
 }
-
