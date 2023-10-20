f611d213f648057fe48e823386686f4d4e48d637
christiancs50 <christian.gaoso@gmail.com>
2023-10-19 17:51:05 -0700

    headerfiles, prototypes and stuct

--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+
 /* heasderfiles */
 
 #include <stdlib.h>
@@ -9,6 +10,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <stdbool.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_ARGUMENTS 50
@@ -18,6 +20,7 @@ int tokenize_command(char *command, char *args[]);
 int str_len(const char *s);
 void shell_prompt(void);
 void run_command(char *args[]);
+void free_buffers(char *buffers[]);
 char *read_user_command(void);
 void _print(const char *message);
 void sig_handler(int signum);
@@ -26,4 +29,13 @@ void env_builtin(void);
 extern char **environ;
 int command_exist(const char *command);
 
+/**
+ * struct identify - defines a stuct named 'idenfify'
+ * @interactive:store a Boolean value
+ * Description:it definnes an instance with the same name
+ */
+struct identify
+{
+	bool interactive;
+} identify;
 #endif /* SHELL_H */
