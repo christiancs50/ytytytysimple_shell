cea74b180307d34ba529b50128c2b9c6fb00b361
christiancs50 <christian.gaoso@gmail.com>
2023-10-19 18:00:36 -0700

    buffer

--- free_buffer.c
+++ free_buffer.c
@@ -1,12 +1,14 @@
 #include "shell.h"
 
-/* free_buffer -
- *
+/**
+ * free_buffers - function that frees memory with array of strings
+ * @buffers: pointer to the string
  */
 
 void free_buffers(char *buffers[])
 {
 	int i = 0;
+
 	while (buffers[i] != NULL)
 	{
 		free(buffers[i]);
